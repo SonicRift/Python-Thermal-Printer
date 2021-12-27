9c91e2967d4498ef4d84f6aefae2633a6cf4170c
Shane Sutro <shane@shanesutro.dev>
2021-12-27 14:14:42 -0700

    Add updated files

--- forecast.py
+++ forecast.py
@@ -5,10 +5,10 @@
 # forecasts for next two days.  See timetemp.py for a different
 # weather example using nice bitmaps.
 # Written by Adafruit Industries.  MIT license.
-# 
+#
 # Required software includes Adafruit_Thermal and PySerial libraries.
 # Other libraries used are part of stock Python install.
-# 
+#
 # Resources:
 # http://www.adafruit.com/products/597 Mini Thermal Receipt Printer
 # http://www.adafruit.com/products/600 Printer starter pack
@@ -40,7 +40,8 @@ def forecast(idx):
     printer.print(deg)
     printer.println(' ' + cond.replace(u'\u2013', '-').encode('utf-8')) # take care of pesky unicode dash
 
-printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+# printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+printer = Adafruit_Thermal()
 deg     = chr(0xf8) # Degree symbol on thermal printer
 
 url = "https://api.darksky.net/forecast/"+API_KEY+"/"+LAT+","+LONG+"?exclude=[alerts,minutely,hourly,flags]&units=us"
